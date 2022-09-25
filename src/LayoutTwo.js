7a267fc092f57e6aad1133ae2a72e39bebe840ec
kareem nour <kimzcodes@gmail.com>
2022-09-24 19:48:17 -0700

    first commit

--- src/LayoutTwo.js
+++ src/LayoutTwo.js
@@ -1,16 +1,14 @@
 function App() {
   return (
-    <div className="wrapper">
-      <div className="header">
-        <h1>Learn code</h1>
+    <div className="wrapper2">
+      <div className="nav">
         <ul>
           <li>home</li>
           <li>faq</li>
           <li>contact</li>
         </ul>
       </div>
-      <div className="main">main</div>
-      <div className="footer">footer</div>
+      <div className="content">content</div>
     </div>
   );
 }
